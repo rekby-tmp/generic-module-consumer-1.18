5a3c259225f693850ae8e25598a847f00bb9e6f4
rekby <rekby@yandex-team.ru>
2022-02-02 00:35:20 +0300

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module generic-module-consumer
+
+go 1.18
+
+require github.com/rekby-tmp/generic-module-producer v0.0.3
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import generic_module_producer "github.com/rekby-tmp/generic-module-producer"
+
+func main() {
+	generic_module_producer.Print(123)
+}
